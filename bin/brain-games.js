88867c821b9dab9449aa3377980ac5b5fa940ef3
Иван <daggerx@mail.ru>
2022-03-10 11:49:35 +0300

    Second step done

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 
 // eslint-disable-next-line no-unused-vars
-//import { userName } from '../src/cli.js';
-console.log('Welcome to the Brain Games!');
+import { userName } from '../src/cli.js';
