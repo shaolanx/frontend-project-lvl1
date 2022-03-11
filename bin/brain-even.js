a9c741ee22d4cfa36de3e4004e2b692e7b84e53c
Иван <daggerx@mail.ru>
2022-03-11 10:53:54 +0300

    Небольшие косметически правки

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line no-unused-vars
 import brainEven from '../src/brain-even-base.js';
 
 brainEven();
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 
-// eslint-disable-next-line import/named
 import Greeting from '../src/cli.js';
 
 Greeting();
